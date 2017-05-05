91d499a4c9ab907ca8039ef19ee661ec9ab3e763
koop011 <leo.sj.yoon@gmail.com>
2017-05-05 19:57:57 +1000

    Update VegetarianPizza.java

--- asgn2/src/asgn2Pizzas/VegetarianPizza.java
+++ asgn2/src/asgn2Pizzas/VegetarianPizza.java
@@ -15,6 +15,10 @@ import asgn2Exceptions.PizzaException;
  *
  */
 public class VegetarianPizza extends Pizza {
+	private int quantity;
+	private LocalTime orderTime;
+	private LocalTime deliveryTime;
+
 
 	/**
 	 * 
@@ -33,6 +37,21 @@ public class VegetarianPizza extends Pizza {
 	 */
 	public VegetarianPizza(int quantity, LocalTime orderTime, LocalTime deliveryTime) throws PizzaException {
 		// TO DO
+		super(quantity, orderTime, deliveryTime, type, price);
+		this.quantity = quantity;
+		this.orderTime = orderTime;
+		this.deliveryTime = deliveryTime;
+		
+		if(this.quantity > maxQuan){
+			throw new PizzaException("Order is out of bounds."); 
+			
+		}
+		else if(this.orderTime.getHour() < minOrderTime){
+			throw new PizzaException("Minimum Time out of bounds.");
+		}
+		else if(this.orderTime.getHour() >= maxOrderTime && this.orderTime.getMinute() != minutes){
+			throw new PizzaException("maximum Time order out of bounds.");
+		}
 	}
 
 }
