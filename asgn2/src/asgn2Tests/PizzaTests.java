e71040cd66054174656cc95412ca58e540c108ba
George <georgefitzgerald718@gmail.com>
2017-05-15 18:07:37 +1000

    Added tests for ordertime and quantity of margherita pizza class

--- asgn2/src/asgn2Tests/PizzaTests.java
+++ asgn2/src/asgn2Tests/PizzaTests.java
@@ -27,12 +27,11 @@ public class PizzaTests {
 
 	@Before 
 	public void createTimes(){
-		ZoneId zone1 = ZoneId.of("Europe/Berlin");
 		LocalTime otime = LocalTime.of(21, 17, 0, 0);
 		LocalTime dtime = LocalTime.of(21, 27, 0, 0);
 	}
 	
-	//Initializes data structure
+	//Initializes data structure on Pizza Abstract class
 	@Test
 	public void initDataStructure() {
 /*
@@ -48,35 +47,94 @@ public class PizzaTests {
 		//assertNotEquals(marg, null);
 	}
 	
-/*	//Throws exception when quantity zero
-	@Test (expected=PizzaException.class)//throws exception when too small quantity
-	public void MargheritaExceptionQuantityZero() throws PizzaException{
-		MargheritaPizza marg = new MargheritaPizza(0, otime, dtime);
-	}
+	//Quantity Exceptions
+
+		//Throws exception when quantity zero
+		@Test (expected=PizzaException.class)//throws exception when too small quantity
+		public void MargheritaExceptionQuantityZero() throws PizzaException{
+			MargheritaPizza marg = new MargheritaPizza(0, otime, dtime);
+		}
+		
+		//Throws exception when quantity > 10
+		@Test (expected=PizzaException.class)//throws exception when too small quantity
+		public void MargheritaExceptionQuantityEleven() throws PizzaException{
+			MargheritaPizza marg = new MargheritaPizza(11, otime, dtime);
+		}
+		
+		//Throws exception when quantity negative
+		@Test (expected=PizzaException.class)//throws exception when too small quantity
+		public void MargheritaExceptionQuantityNeg() throws PizzaException{
+			MargheritaPizza marg = new MargheritaPizza(-1, otime, dtime);
+		}
+		
+		//Doesn't throw exception when quantity is one
+		public void MargheritaQuantityOne() throws PizzaException{
+			MargheritaPizza marg = new MargheritaPizza(1, otime, dtime);
+			assertNotEquals(marg, null);
+		}
+		
+		//Doesn't throw exception when quantity is ten
+		public void MargheritaQuantityTen() throws PizzaException{
+			MargheritaPizza marg = new MargheritaPizza(10, otime, dtime);
+			assertNotEquals(marg, null);
+		}
 	
-	//Throws exception when quantity > 10
-	@Test (expected=PizzaException.class)//throws exception when too small quantity
-	public void MargheritaExceptionQuantityEleven() throws PizzaException{
-		MargheritaPizza marg = new MargheritaPizza(11, otime, dtime);
-	}
+	//Ordertime Exceptions
+		
+		//Throws exception if ordertime less than min order time of 18
+		@Test (expected=PizzaException.class)
+		public void MargheritaExceptionOrdertime17() throws PizzaException{
+			otime = LocalTime.of(17, 00, 0, 0);
+			MargheritaPizza marg = new MargheritaPizza(1, otime, dtime);
+		}
 	
-	//Throws exception when quantity negative
-	@Test (expected=PizzaException.class)//throws exception when too small quantity
-	public void MargheritaExceptionQuantityNeg() throws PizzaException{
-		MargheritaPizza marg = new MargheritaPizza(-1, otime, dtime);
-	}*/
+		//Throws exception if ordertime just less than min order time of 18
+		@Test (expected=PizzaException.class)
+		public void MargheritaExceptionOrdertime175959() throws PizzaException{
+			otime = LocalTime.of(17, 59, 59, 0);
+			MargheritaPizza marg = new MargheritaPizza(1, otime, dtime);
+		}
+		//Don't throw exception if ordertime is 18
+		@Test 
+		public void MargheritaExceptionOrdertime18() throws PizzaException{
+			otime = LocalTime.of(18, 00, 0, 0);
+			MargheritaPizza marg = new MargheritaPizza(1, otime, dtime);
+		}
 	
-	//Throws exception if ordertime < min order time
+		//Throws exception if ordertime greater than order time of 22
+		@Test (expected=PizzaException.class)
+		public void MargheritaExceptionOrdertime23() throws PizzaException{
+			otime = LocalTime.of(23, 00, 0, 0);
+			MargheritaPizza marg = new MargheritaPizza(1, otime, dtime);
+		}
+		
+		//Throws exception if ordertime is 22
+		@Test (expected=PizzaException.class)
+		public void MargheritaExceptionOrdertime22() throws PizzaException{
+			otime = LocalTime.of(22, 00, 0, 0);
+			MargheritaPizza marg = new MargheritaPizza(1, otime, dtime);
+		}
+		
+		//Throws exception if ordertime greater than order time of 22 by minutes
+		@Test (expected=PizzaException.class)
+		public void MargheritaExceptionOrdertime2201() throws PizzaException{
+			otime = LocalTime.of(22, 01, 0, 0);
+			MargheritaPizza marg = new MargheritaPizza(1, otime, dtime);
+		}
 	
+		//Throws exception if ordertime greater than order time of 22 by second
+		@Test (expected=PizzaException.class)
+		public void MargheritaExceptionOrdertime220001() throws PizzaException{
+			otime = LocalTime.of(22, 00, 01, 0);
+			MargheritaPizza marg = new MargheritaPizza(1, otime, dtime);
+		}
 	
-	
-	
-	
-	
-	
-	
-	
-	
+		//Doesn't throw exception when order time is 11pm 
+		@Test (expected=PizzaException.class)
+		public void MargheritaExceptionOrdertime215959() throws PizzaException{
+			otime = LocalTime.of(21, 59, 59, 0);
+			MargheritaPizza marg = new MargheritaPizza(1, otime, dtime);
+		}
 	
 	
 	
