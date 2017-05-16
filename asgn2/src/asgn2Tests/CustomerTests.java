b64b64d767529b1f68d63eceb4ed79ff6a090f37
koop011 <leo.sj.yoon@gmail.com>
2017-05-16 11:20:47 +1000

    Added customer test

--- asgn2/src/asgn2Tests/CustomerTests.java
+++ asgn2/src/asgn2Tests/CustomerTests.java
@@ -11,5 +11,64 @@ package asgn2Tests;
  *
  */
 public class CustomerTests {
-	// TO DO
+	PickUpCustomer test1;
+	PickUpCustomer cus1;
+	PickUpCustomer cus2;
+	
+	
+	@Before 
+	public void initialize() throws CustomerException{
+		cus1 = new PickUpCustomer("Leo", "0433563076",5,4);
+	}
+	
+	
+	@Test (expected=CustomerException.class)
+	public void noNameTest() throws CustomerException{
+		test1 = new PickUpCustomer("", "0123456789",1,2);
+	}
+	
+	@Test (expected=CustomerException.class)
+	public void tooMuchLettersName() throws CustomerException{
+		test1 = new PickUpCustomer("This has got to be the longest name in the world because it's this long", "1234567890",1,2);
+	}
+	
+	@Test (expected=CustomerException.class)
+	public void numberNotEnough() throws CustomerException{
+		test1 = new PickUpCustomer("Leo", "0123",1,2);
+	}
+	
+	@Test
+	public void getNameTest(){
+		String test = cus1.getName();
+		assertEquals("Leo",test);
+	}
+	
+	@Test
+	public void getMobileTest(){
+		String test = cus1.getMobileNumber();
+		assertEquals("0433563076", test);
+	}
+	
+	@Test
+	public void getCusType(){
+		String test = cus1.getCustomerType();
+		assertEquals("PickUp", test);
+	}
+	
+	@Test
+	public void getLocationXTest(){
+		int x = cus1.getLocationX();
+		assertEquals(5,x);
+	}
+	
+	@Test
+	public void getLocationYTest(){
+		int y = cus1.getLocationY();
+		assertEquals(4,y);
+	}
+	
+	@Test
+	public void getDeliveryDistanceTest(){
+		
+	}
 }
