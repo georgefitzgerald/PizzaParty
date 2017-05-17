11abb8766b283b7f52d7f70a354904858b6c624d
koop011 <leo.sj.yoon@gmail.com>
2017-05-17 13:26:22 +1000

    Added in basic initial objects to test Customer Factory

--- asgn2/src/asgn2Tests/CustomerFactoryTests.java
+++ asgn2/src/asgn2Tests/CustomerFactoryTests.java
@@ -8,4 +8,23 @@ package asgn2Tests;
  */
 public class CustomerFactoryTests {
 	// TO DO
+	PickUpCustomer test1;
+	
+	PickUpCustomer cus1;
+	
+	DriverDeliveryCustomer test2;
+	
+	DroneDeliveryCustomer test3;
+	
+	
+	@Before 
+	public void initialize() throws CustomerException{
+		cus1 = new PickUpCustomer("Leo", "0433563076",5,4);
+		test2 = new DriverDeliveryCustomer("George", "0402123456", 5,3);
+	}
+	
+	@Test
+	public void cusFactoryTest(){
+		
+	}
 }
