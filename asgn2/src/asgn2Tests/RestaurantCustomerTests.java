f61c0d6140cfaf317369863aedc70e54ed143b38
koop011 <leo.sj.yoon@gmail.com>
2017-05-22 16:32:42 +1000

    Added most test cases for Customer in PizzaRestaurant

--- asgn2/src/asgn2Tests/RestaurantCustomerTests.java
+++ asgn2/src/asgn2Tests/RestaurantCustomerTests.java
@@ -9,4 +9,95 @@ package asgn2Tests;
  */
 public class RestaurantCustomerTests {
 	// TO DO
+	PizzaRestaurant cusRes;
+	
+	@Before //read in data structure
+	public void RestaurantInstance() throws CustomerException, PizzaException, LogHandlerException{
+		cusRes = new PizzaRestaurant();
+		cusRes.processLog("C:\\Users\\You\\Documents\\PizzaParty\\asgn2\\logs\\20170101.txt");
+	}
+	
+	//Constructor initialization
+	//Creates an instance of object
+	@Test
+	public void CreateRestaurantInstance(){
+		PizzaRestaurant cusRes1 = new PizzaRestaurant();
+		assertNotEquals(cusRes1.toString(), null);
+	}
+	
+	//separate instance created
+	@Test 
+	public void SeparateInstances(){
+		PizzaRestaurant cusRes1 = new PizzaRestaurant();
+		PizzaRestaurant cusRes2 = new PizzaRestaurant();
+		assertNotEquals(cusRes1.toString(), cusRes2.toString());
+	}
+	
+	//process log
+	//Loghandler exception thrown when file doesn't exist
+	@Test (expected=LogHandlerException.class)
+	public void FileDoesntExistException() throws CustomerException, PizzaException, LogHandlerException{
+		cusRes.processLog("");
+	}
+	
+	//Customer Exception thrown when customer data is incorrect (10<quantity<1, time)
+		//need my own log file for this. 
+	//calls populatecustomerdataset - check all customers at index aren't empty
+	@Test
+	public void populatesCustomerDataSet(){
+		assertEquals(cusRes.getNumCustomerOrders(), 3);
+	}
+	
+	//returns true when everythings correct
+	@Test
+	public void returnsTrueWhenSucces() throws CustomerException, PizzaException, LogHandlerException{
+		//cusRes = new PizzaRestaurant();
+		String filename = "C:\\Users\\You\\Documents\\PizzaParty\\asgn2\\logs\\20170101.txt";
+		assertEquals(cusRes.processLog(filename), true);
+	}
+	
+	//Get customer by index
+	//gets correct customer
+	@Test
+	public void getsCorrectCustomer() throws CustomerException{
+		assertEquals(cusRes.getCustomerByIndex(0).getCustomerType(), "DriverDelivery");
+		assertEquals(cusRes.getCustomerByIndex(0).getName(), "Casey Jones");
+	}
+	//doesn't return the same customer 
+	@Test
+	public void createsSeparateCustomer() throws CustomerException{
+		assertNotEquals(cusRes.getCustomerByIndex(0), cusRes.getCustomerByIndex(1));
+		assertNotEquals(cusRes.getCustomerByIndex(1), cusRes.getCustomerByIndex(2));
+		assertNotEquals(cusRes.getCustomerByIndex(0), cusRes.getCustomerByIndex(2));
+	}
+	
+	//Get customer location
+	//Log1
+	@Test
+	public void customerLocationLog1() throws CustomerException{
+		double loc = Math.abs(cusRes.getCustomerByIndex(0).getLocationX() + Math.abs(cusRes.getCustomerByIndex(0).getLocationY()));
+		assertNotEquals(cusRes.getCustomerByIndex(0).getDeliveryDistance(), loc);
+	}
+
+	//Log2
+	@Test
+	public void customerLocationLog2() throws CustomerException, PizzaException, LogHandlerException{
+		cusRes.processLog("C:\\Users\\You\\Documents\\PizzaParty\\asgn2\\logs\\20170102.txt");
+		double loc = 0;
+		assertNotEquals(cusRes.getCustomerByIndex(3).getDeliveryDistance(), loc);
+	}
+	
+	//Log3
+	@Test
+	public void customerLocationLog3() throws CustomerException, PizzaException, LogHandlerException{
+		cusRes.processLog("C:\\Users\\You\\Documents\\PizzaParty\\asgn2\\logs\\20170103.txt");
+		double loc = Math.sqrt((Math.pow(cusRes.getCustomerByIndex(2).getLocationX(), 2))+(Math.pow(cusRes.getCustomerByIndex(2).getLocationY(), 2)));
+		assertNotEquals(cusRes.getCustomerByIndex(2).getDeliveryDistance(), loc);
+	}
+	
+	//get Total delivery distance
+	@Test
+	public void totalDeliveryDistanceLog1(){
+		
+	}
 }
